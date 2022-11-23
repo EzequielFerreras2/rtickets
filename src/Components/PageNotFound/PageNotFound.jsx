3c87865e2e0604d843712bda727e0002ffca367a
Ezequiel Ferrreras <52600831+EzequielFerreras2@users.noreply.github.com>
2022-11-23 16:18:24 -0400

    404

--- src/Components/PageNotFound/PageNotFound.jsx
+++ src/Components/PageNotFound/PageNotFound.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const PageNotFound = () => {
     return (
         <>
-            <h1>PageNotFound</h1>
+        <br/>
+            <img src='https://static.vecteezy.com/system/resources/previews/001/812/604/non_2x/page-not-found-404-design-illustration-404-error-web-page-concept-minimal-cartoon-flat-style-vector.jpg'/>
         </>
     );
 }
