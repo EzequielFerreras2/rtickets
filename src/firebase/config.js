7480328fc873ce8c82c3ac50279c39748597e04d
Ezequiel Ferrreras <52600831+EzequielFerreras2@users.noreply.github.com>
2022-11-25 17:10:54 -0400

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from 'firebase/auth'
+import {getFirestore} from 'firebase/firestore/lite'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRsYEVx8TFg-05U2AWYrpyeB319ji5p1M",
+  authDomain: "rticketsapp.firebaseapp.com",
+  projectId: "rticketsapp",
+  storageBucket: "rticketsapp.appspot.com",
+  messagingSenderId: "994399824533",
+  appId: "1:994399824533:web:6df3326811b08af45e099c",
+  measurementId: "G-SH8R1MFCBT"
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebaseDB = getFirestore(FirebaseApp);
+
+const analytics = getAnalytics(FirebaseApp);
